261c36994d8a7d58ceba192723cd94767c984b56
root <kNosek1618>
2019-07-12 21:27:30 +0000

    unlucky numbers create code

---
+++ UnluckyNumbers.py
@@ -0,0 +1,12 @@
+
+x = 0
+
+for n in range(20):
+    x = x + 1
+    if x != 4 and x != 13:
+        if x % 2 != 0:
+            print(f"{x} is odd")
+        elif x % 2 == 0:
+            print(f"{x} is even")
+    else:
+        print(f"{x} is UNLUCKY!")
