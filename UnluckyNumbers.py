c4b951e12c01e921a9b3ca6dca49a1af1c44851c
root <kNosek1618>
2019-07-12 21:29:58 +0000

    upgrade code

--- UnluckyNumbers.py
+++ UnluckyNumbers.py
@@ -6,7 +6,7 @@ for n in range(20):
     if x != 4 and x != 13:
         if x % 2 != 0:
             print(f"{x} is odd")
-        elif x % 2 == 0:
+        else:
             print(f"{x} is even")
     else:
         print(f"{x} is UNLUCKY!")
