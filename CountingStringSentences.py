410396fcf04a9aeca0d22c79860b445749888122
root <kNosek1618>
2019-07-13 19:21:01 +0000

    new app.py

--- CountingStringSentences.py
+++ CountingStringSentences.py
@@ -1,7 +1,7 @@
 
 times = int(input("How many times do I have to tell you? "))
 
-for time in range(times):
+for time in range(1,times):
     print("CLEAN UP YOUR ROOM !")
     if time >= 4:
         print("do you even listen anymore?")
